8f7cef599032c7e1d466a05ae27a118950199e33
marcusungarette <marcusungarette@gmail.com>
2019-11-24 21:30:55 -0300

    blocking admin from non localhost

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const dbConnection = sqlite.open(path.resolve(__dirname,'banco.sqlite'), { Promi
 
 const port = process.env.PORT || 3000
 
+app.use('/admin', (req, res, next) => {   
+    if(req.hostname === 'localhost'){
+        next()
+    }else{
+        res.send('Not allowed')
+    }
+    
+})
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs') /*como view engine vamos utilizar o ejs (npm)*/
 app.use(express.static(path.join(__dirname, 'public'))) /*se nao encontrar '/' ele busca oque tiver na pasta public */
